19bfada47be3bd916421f9f802d5bffe4a4c00b1
Emad Coder <165240908+emadcode@users.noreply.github.com>
2024-05-27 19:37:00 +0300

    Add files via upload

---
+++ oop2/script.js
@@ -0,0 +1,25 @@
+const person = {
+    name: 'Emad',
+    age: 22,
+    job: 'Developer',
+    address: {
+      city: 'Egypt',
+      country: 'cairo'
+    }
+  };
+  
+  const displayPersonInfo = ({ name, age, job, address: { city, country } }) => {
+    console.log(`Name: ${name}, Age: ${age}, Job: ${job}`);
+    console.log(`City: ${city}, Country: ${country}`);
+  };
+  
+  displayPersonInfo(person);
+  
+  // Using rest operator and default values
+  const { name: fullName, address: { city: town, state = 'Unknown' }, ...otherInfo } = person;
+  
+  console.log(fullName); // Emad
+  console.log(town); // cairo
+  console.log(state); // Unknown
+  console.log(otherInfo); // { age: 30, job: 'Developer' }
+  
